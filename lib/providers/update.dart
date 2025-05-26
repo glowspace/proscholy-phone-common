c2935450e981c29fefaa50846ffd231267bf60c2
Patrik Dobiáš <patrikdobidobias@gmail.com>
2025-05-26 12:19:24 +0200

    fix imports

--- lib/providers/update.dart
+++ lib/providers/update.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:graphql_flutter/graphql_flutter.dart' hide Store;
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:proscholy_common/constants.dart';
